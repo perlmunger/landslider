f118a31e770cde978db2842f15ec98cfe25b1d83
Jay Prall <jayprall@gmail.com>
2011-04-25 22:39:17 -0400

    add tests for get_account_notes and get_account_opportunities

--- test/landslider_test.rb
+++ test/landslider_test.rb
@@ -21,7 +21,7 @@ class LandsliderTest < Test::Unit::TestCase
 		assert_operator result[:revision_version], :>=, 0
 	end
 	
-	def test_get_accounts
+	def test_landslider_get_accounts
 		result = Landslider.login('LOGINTOKEN=' + LS_INSTANCE_NAME)
 		result = Landslider.get_accounts(result[:session_id])
 		assert_equal false, result[:error]
@@ -32,8 +32,53 @@ class LandsliderTest < Test::Unit::TestCase
 		assert_not_nil result[:result_msg]
 	end
 	
+	def test_landslider_get_account_contacts
+		result = Landslider.login('LOGINTOKEN=' + LS_INSTANCE_NAME)
+		
+		# exists on jaytest
+		result = Landslider.get_account_contacts(result[:session_id], 51857822)		
+		
+		assert_equal false, result[:error]
+		assert_operator result[:results_returned], :>=, 1
+		assert result[:contacts].all? { |con| !con[:last_name].nil? }, "last name required"
+	end
+	
+	def test_landslider_get_account_notes
+		result = Landslider.login('LOGINTOKEN=' + LS_INSTANCE_NAME)
+
+		# exists on jaytest
+		result = Landslider.get_account_notes(result[:session_id], 55647822)
+		
+		validate_standard_api_result result
+		assert_operator result[:results_returned], :>=, 1
+
+	end
+	
+	
+	def test_landslider_get_account_opportunities
+		result = Landslider.login('LOGINTOKEN=' + LS_INSTANCE_NAME)
+
+		# exists on jaytest
+		result = Landslider.get_account_opportunities(result[:session_id], 51858821)
+	  
+		assert_equal false, result[:error]
+		assert result[:opportunities].all? { |opp| 
+			!opp[:account_name].nil? && 
+			!opp[:name].nil? && 
+			!opp[:deal_value].nil? && 
+			!opp[:selling_process].nil?
+		}, "opportunities require a name, account, deal value and selling process"
+  
+  end
+	
 
 	private
+
+	def validate_standard_api_result(result) 
+		assert_equal Hash, result.class, "api method should return a hash"
+		assert_operator result[:results_returned], :>=, 0
+		assert_equal false, result[:error], "error should be false"
+	end
 
 	def login(session_id)
 		Landslider.login(session_id)
