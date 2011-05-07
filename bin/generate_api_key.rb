ffb9039f397d2f04adadfe9a90c1109cc97668e3
Jay Prall <jayprall@gmail.com>
2011-05-07 15:39:55 -0400

    update doc

--- bin/generate_api_key.rb
+++ bin/generate_api_key.rb
@@ -2,7 +2,7 @@
 require 'digest/md5'
 
 LS_INSTANCE_NAME = 'jaytest'
-LS_API_PASSWORD = 'Applogin1'
+LS_API_PASSWORD = 'letmein52`'
 
 # the api key is an md5 hash of the user password + instance name
 puts "LS_API_KEY = '#{Digest::MD5.hexdigest(LS_API_PASSWORD + LS_INSTANCE_NAME)}'"
