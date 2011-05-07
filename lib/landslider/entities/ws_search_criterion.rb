15ef9c1ace9ff8f41bbb8f44b324cefe5129ba1c
Jay Prall <jayprall@gmail.com>
2011-05-07 17:05:08 -0400

    doc

--- lib/landslider/entities/ws_search_criterion.rb
+++ lib/landslider/entities/ws_search_criterion.rb
@@ -8,11 +8,25 @@ class Landslider
 		
 		# @param [String] field_id 
 		# Currently Searchable Account Fields:
-		# 	[AccountId, AccountName, AccountNumber, AccountShortName, AccountType, AccountTypeId, ArchivedOn, BillingAddressCity, BillingAddressCountry, BillingAddressState, BillingAddressStreet, BillingAddressZip, CreatedById, CreatedDate, Division, Fax, LastUpdatedById, LastUpdatedDate, MainAddressCity, MainAddressCountry, MainAddressState, MainAddressStreet, MainAddressZip, NewsKeywords, OwnerId, ParentAccount, ParentAccountId, Phone, PrimaryContactId, QuickbooksLastSynced, ShippingAddressCity, ShippingAddressCountry, ShippingAddressState, ShippingAddressStreet, ShippingAddressZip, SourceSystemId, SyncWithQuickBooks, Ticker, Url]
+		# * AccountId
+		# * AccountName
+		# * AccountNumber
+		# * AccountShortName
+		# * AccountType
+		# * AccountTypeId
+		# * ArchivedOn
+		# * BillingAddressCity
+		# * BillingAddressCountry
+		# * BillingAddressState
+		# * BillingAddressStreet, BillingAddressZip, CreatedById, CreatedDate, Division, Fax, LastUpdatedById, LastUpdatedDate, MainAddressCity, MainAddressCountry, MainAddressState, MainAddressStreet, MainAddressZip, NewsKeywords, OwnerId, ParentAccount, ParentAccountId, Phone, PrimaryContactId, QuickbooksLastSynced, ShippingAddressCity, ShippingAddressCountry, ShippingAddressState, ShippingAddressStreet, ShippingAddressZip, SourceSystemId, SyncWithQuickBooks, Ticker, Url]
+		#
 		# Currently Searchable Opportunity Fields:
-		# 	[AccountId, AccountName, ArchivedOn, ClosedDate, ComputedConfidence, ComputedForecastDate, CreatedById, CreatedDate, CurrentPhaseId, CurrentPhaseName, DealValue, Description, IsHot, LastUpdatedById, LastUpdatedDate, LeadSource, LeadSourceId, Name, OpportunityId, OverriddenConfidence, OverriddenForecastDate, OverrideComments, OwnerId, PaymentTerms, PaymentTermsId, QuickbooksLastSynced, QuickbooksTransactionType, SellingProcess, SellingProcessId, SourceSystemId, StartDate, Status, StatusId, StatusReason, StatusReasonId, SuspendedDate]
+		# * AccountId
+		# * AccountName, ArchivedOn, ClosedDate, ComputedConfidence, ComputedForecastDate, CreatedById, CreatedDate, CurrentPhaseId, CurrentPhaseName, DealValue, Description, IsHot, LastUpdatedById, LastUpdatedDate, LeadSource, LeadSourceId, Name, OpportunityId, OverriddenConfidence, OverriddenForecastDate, OverrideComments, OwnerId, PaymentTerms, PaymentTermsId, QuickbooksLastSynced, QuickbooksTransactionType, SellingProcess, SellingProcessId, SourceSystemId, StartDate, Status, StatusId, StatusReason, StatusReasonId, SuspendedDate]
+		#
 		# Currently Searchable Duration Type Fields:
-		# 	[DurationType, DurationTypeId]
+		# * DurationType
+		# * DurationTypeId]
 		# Currently Searchable Product Family Fields:
 		# 	[Description, ParentProductFamily, ParentProductFamilyId, ProductFamily, ProductFamilyId]
 		# @param [String, WsSearchOperator] operator Contains,SoundsLike,Equals,NotEquals,LessThan,LessThanOrEqual,GreaterThan,GreaterThanOrEqual,Empty,NotEmpty,True,False,In,NotIn
