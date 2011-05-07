eaf4a1e564fe229c80ad5b64d98884e9c7eb98b8
Jay Prall <jayprall@gmail.com>
2011-05-07 17:30:26 -0400

    doc

--- lib/landslider/entities/ws_search_operator.rb
+++ lib/landslider/entities/ws_search_operator.rb
@@ -2,25 +2,36 @@
 class Landslider
 	
 	class WsSearchOperator
-	
-		DEFAULT='Default'
+
+		# returns values that contain the given search string
 		CONTAINS='Contains'
+		# returns values that “sound like” the given search string (smythe = smith)
+		SOUNDS_LIKE='SoundsLike'
 		STARTS_WITH='StartsWith'
 		ENDS_WITH='EndsWith'
 		DOES_NOT_CONTAIN='DoesNotContain'
-		# EQUALS is an rdoc test
+		# returns values that are identically equal (case-insensitive) the given search value
 		EQUALS='Equals'
+		# returns values that are not  equal (case-insensitive) the given search value
 		NOT_EQUALS='NotEquals'
-		# test less than
 		LESS_THAN='LessThan'
+		# returns values that are less than or equal to the given search value
 		LESS_THAN_OR_EQUAL='LessThanOrEqual'
+		# returns values that are greater than the given search value
 		GREATER_THAN='GreaterThan'
+		# returns values that are greater than or equal to the given search value
 		GREATER_THAN_OR_EQUAL='GreaterThanOrEqual'
+		# returns values that are empty (null)
 		EMPTY='Empty'
+		# returns values that are not empty (not null)
 		NOT_EMPTY='NotEmpty'
+		# returns values that are logically True
 		TRUE='True'
+		# returns values that are logically False
 		FALSE='False'
+		# returns values that are in the specified list
 		IN='In'
+		# returns values that are not in the specified list
 		NOT_IN='NotIn'
 	end
 	
