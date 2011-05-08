584b42dcf669b03e4c4843c0f6e1b00392cdb5f5
Jay Prall <jayprall@gmail.com>
2011-05-07 21:57:33 -0400

    doc

---
+++ lib/landslider/entities/ws_employee.rb
@@ -0,0 +1,23 @@
+
+class Landslider
+
+	class WsEmployee < WsEntity
+		
+		# @return [Integer]
+		attr_reader :employee_id
+		
+		# @return [String]
+		attr_reader :cell_phone, :company_name, :email_address, :fax
+
+		# @return [String]
+		attr_reader :first_name, :home_phone, :last_name, :office_phone
+		
+		# @return [Streing]
+		attr_reader :titl, :user_id
+		
+		# @return [Boolean]
+		attr_reader :administrator
+		
+	end
+	
+end
