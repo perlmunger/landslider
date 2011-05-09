986d762fb734110941b3341322ccce8bb043c150
Jay Prall <jayprall@gmail.com>
2011-05-08 21:23:30 -0400

    rdoc

--- lib/landslider/entities/ws_account.rb
+++ lib/landslider/entities/ws_account.rb
@@ -7,36 +7,26 @@ class Landslider
 		attr_reader :account_id, :account_owner, :archived_by, :create_by, :update_by, :parent_account_id
 		
 		# @return [String]
-		attr_reader :account_name, :account_number, :division
+		attr_reader :account_name, :account_number, :division, :fax
+		
+		# @return [String]
+		attr_reader :phone, :source_id, :url
 
 		# @return [Date]
 		attr_reader :created_on, :updated_on
 		
+		# @return [WsAccountType]
+		attr_reader :account_type
+		
+		# @return [WsAddress]
+		attr_reader :billing_address, :main_address, :shipping_address
+		
+		# @return [WsContact]
+		attr_reader :primary_contact
+		
+		# @return WsAccountCustomField[]
+		attr_reader :custom_fields
+				
 	end
 	
 end
-	# private String accountName;
-	# private String accountNumber;
-	# private WsAccountType accountType;
-	# private Date archivedOn;
-	# private WsAddress billingAddress;
-	# private int createBy;
-	# private Date createdOn;
-	# private int updateBy;
-	# private Date updatedOn;
-	# private String division;
-	# private String fax;
-	# private WsAddress mainAddress;
-	# private String newsKeywords;
-	# private String phone;
-	# private WsAddress shippingAddress;
-	# private String shortName;
-	# private String sourceId;
-	# private String ticker;
-	# private String url;
-	# private List<WsAccountCustomField> _customFields = new ArrayList<WsAccountCustomField>();
-	# private int parentAccountId;
-	# private String parentAccountName;
-	# private boolean isSyncWithQuickbooks;
-	# private Date quickbooksLastSynced;
-	# private WsContact primaryContact;
