9e498c6bee7d2bddb5b445122795fcc04545a397
Jay Prall <jayprall@gmail.com>
2011-05-08 21:05:34 -0400

    add WsLeadContact

---
+++ lib/landslider/entities/ws_lead_contact.rb
@@ -0,0 +1,14 @@
+
+class Landslider
+
+	class WsLeadContact < WsEntity
+		
+		# @return [Integer]
+		attr_reader :contact_id, :lead_id, :modified_by
+		
+		# @return [Boolean]
+		attr_reader :primary_contact
+		
+	end
+	
+end
