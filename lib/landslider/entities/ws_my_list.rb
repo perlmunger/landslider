8716a8c255db5643ea93a2ce845d5948b29805db
Jay Prall <jayprall@gmail.com>
2011-05-08 21:10:29 -0400

    add WsMyList

---
+++ lib/landslider/entities/ws_my_list.rb
@@ -0,0 +1,20 @@
+
+class Landslider
+
+	class WsMyList < Object
+		
+		# @return [Integer]
+		attr_reader :my_list_id, :created_by
+		
+		# @return [Strin]
+		attr_reader :title, :description, :base_table
+		
+		# @return [Date]
+		attr_reader :created_on
+		
+		# @return [Boolean]
+		attr_reader :is_shared
+		
+	end
+	
+end
