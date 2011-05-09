c895bd2e438ffccb849d332cb0416179fea4ae70
Jay Prall <jayprall@gmail.com>
2011-05-08 21:32:21 -0400

    Ws pick list itm

---
+++ lib/landslider/entities/ws_pick_list_item.rb
@@ -0,0 +1,14 @@
+
+class Landslider
+
+	class WsPickListItem < Object
+		
+		# @return [Integer]
+		attr_reader :id, :list_type_id
+		
+		# @return [String]
+		attr_reader :display_string
+		
+	end
+	
+end
