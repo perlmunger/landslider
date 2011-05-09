418936f68137c9229a6710062968b28acf8f0d25
Jay Prall <jayprall@gmail.com>
2011-05-08 21:26:15 -0400

    WsSellingProcess

---
+++ lib/landslider/entities/ws_selling_process.rb
@@ -0,0 +1,17 @@
+
+class Landslider
+
+	class WsSellingProcess < Object
+		
+		# @return [Integer]
+		attr_writer :process_id
+		
+		# @return [String]
+		attr_writer :process_name
+		
+		# @return [WsEntityType]
+		attr_writer :entity_type
+		
+	end
+	
+end
