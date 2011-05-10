bd2b4f5192f042ee7e0543cad5e8332dc7d949b7
Jay Prall <jayprall@gmail.com>
2011-05-10 19:44:23 -0400

    include entities

--- lib/landslider/entities.rb
+++ lib/landslider/entities.rb
@@ -1,21 +1,36 @@
 require 'landslider/entities/ws_entity'
-require 'landslider/entities/ws_employee'
-
+require 'landslider/entities/ws_result'
+require 'landslider/entities/ws_primary_entity'
 
 require 'landslider/entities/ws_search'
-require 'landslider/entities/ws_contact_search'
-
-require 'landslider/entities/ws_contact_search'
-require 'landslider/entities/ws_payment_term'
-
-
 require 'landslider/entities/ws_search_criterion'
 require 'landslider/entities/ws_search_operator'
 
+require 'landslider/entities/ws_account'
+require 'landslider/entities/ws_account_note'
 require 'landslider/entities/ws_account_note_search'
+require 'landslider/entities/ws_address'
+
+require 'landslider/entities/ws_contact'
+require 'landslider/entities/ws_contact_note'
 require 'landslider/entities/ws_contact_note_search'
+require 'landslider/entities/ws_contact_search'
+require 'landslider/entities/ws_employee'
+
+require 'landslider/entities/ws_lead'
+require 'landslider/entities/ws_lead_contact'
+require 'landslider/entities/ws_lead_note'
 require 'landslider/entities/ws_lead_note_search'
+require 'landslider/entities/ws_lead_search'
+
+require 'landslider/entities/ws_my_list'
+require 'landslider/entities/ws_note'
+require 'landslider/entities/ws_opportunity'
+require 'landslider/entities/ws_opportunity_note'
 require 'landslider/entities/ws_opportunity_note_search'
 require 'landslider/entities/ws_opportunity_status'
 
-require 'landslider/entities/ws_result'
+
+require 'landslider/entities/ws_payment_term'
+
+
