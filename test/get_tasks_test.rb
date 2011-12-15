bb5537fd7fef3d6df8cee62524e66fb2af72a7a2
Jay Prall <jayprall@gmail.com>
2011-12-15 15:25:41 -0500

    cleanup

--- test/get_tasks_test.rb
+++ test/get_tasks_test.rb
@@ -11,9 +11,6 @@ class GetTasksTest < Test::Unit::TestCase
 	def test_get_accounts_without_search
 		search = Landslider::WsTaskSearch.new
 		result = Landslider.get_tasks($sesson_idt7, search)
-
-		puts result.inspect
-
 		assert_not_nil result
 		assert_equal false, result[:error]
 		assert_kind_of Array, result[:tasks]
